342725c8f58ecc591d013bdd4b03c5689e493b66
davidduran94 <davidduran94@gmail.com>
2021-05-25 22:22:47 -0500

    added polyfil

--- src/components/listOfCategories/index.js
+++ src/components/listOfCategories/index.js
@@ -1,18 +1,60 @@
-import React from 'react'
-import {Category} from '../Category'
-import {List, Item} from './styles'
-import {categories}  from '../../../api/db.json'
-export const ListOfCategories = () => {
-    return (
-        <List>
-            {
-                categories.map((cat) => (
-                    <Item key={cat.id}>
-                        <Category {...cat} />
-                    </Item>
-                  ))
+import React, { Fragment, useEffect, useState } from 'react'
+import { Category } from '../Category'
+import { categories as categoriesApi} from '../../../api/db.json'
+import { List, Item } from './styles'
 
-            }
-        </List>
-    )
+//custom  HOOK
+function useCategoriesData() {
+    const [categories, setCategories] = useState([])
+    const [loading, setLoading] = useState(false)
+    useEffect(function () {
+        // window.fetch('https://petgram-server.midudev.now.sh/categories')
+        //   .then(res => res.json())
+        //   .then(response => {
+        //     setCategories(response)
+        //   })
+        console.log(categoriesApi)
+        setLoading(true)
+        setTimeout( () => {
+            setCategories(categoriesApi) 
+            setLoading(false) 
+         } , 500);
+      }, [])
+    
+    return {categories, loading}
+}
+
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
+  
+  useEffect(function () {
+    const onScroll = e => {
+        const newShowFixed = window.scrollY > 200
+        showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
+      {
+        loading ?
+        'Loading....'
+        : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+      }
+    </List>
+  )
+
+
+  return (
+    <Fragment>
+      {renderList()} 
+      {showFixed && renderList(true)}
+    </Fragment>
+  )
 }
